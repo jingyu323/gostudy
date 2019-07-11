d1d81aa852d18600ed0f5a82617d60ecf1bd734e
Rain <xzg323@163.com>
2019-07-11 23:30:37 +0800

    添加 goServer端

--- test/hello.go
+++ test/hello.go
@@ -2,7 +2,18 @@ package main
 
 import "fmt" //系统包用来输出的
 
+//常量定义区
+
+const NAME = "name "
+
+/*
+
+ */
 func main() {
 	// 打印函数调用语句。用于打印输出信息。
 	fmt.Println("hello world")
+
+	var a int = 65
+	fmt.Println(a)
+
 }
