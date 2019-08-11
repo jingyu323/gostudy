f98dcedf353e4ec6824b6fa0bbf8890cee72ee5e
Rain <xzg323@163.com>
2019-08-11 11:15:06 +0800

    字符串 读和格式化 字符串截取

--- test/fileread.go
+++ test/fileread.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io/ioutil"
 	"os"
+	"strings"
 )
 
 // os.Open
@@ -63,17 +64,37 @@ func read3(filePath string) string {
 	}
 
 	f, _ := ioutil.ReadAll(file)
+
 	text := string(f)
+
 	return text
+
+} //ioutil
+func read4(filePath string) string {
+
+	result := ""
+	if contents, err := ioutil.ReadFile(filePath); err == nil {
+		//直接使用\n根本无法替换成功，正确的做法是对\n再进行转义：\\n
+		result = strings.Replace(string(contents), "\\n", "", -1)
+		fmt.Print("Use ioutil.ReadFile to read a file:", result)
+	}
+
+	return result
 
 }
 
 func main() {
 	content := read1("./resource/testfile.txt")
 	content2 := read3("./resource/testfile.txt")
+	content3 := read4("./resource/testfile.txt")
 
 	fmt.Printf(content)
 	fmt.Println()
 	fmt.Println("************")
+
 	fmt.Printf(content2)
+	fmt.Println()
+	fmt.Println("************")
+	fmt.Printf("result=" + content3)
+
 }
---
+++ test/string_format.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+const sourceStr string = "hello world"
+
+type person struct {
+	name string
+	age  int
+}
+
+func main() {
+
+	fmt.Println(strings.Contains(sourceStr, "ll"))
+
+	//统计子符串的次数
+	fmt.Println(strings.Count("hello hello", "ll"))
+	//判断前缀字符串
+	fmt.Println(strings.HasPrefix("hello", "he"))
+	//判断后缀字符串
+	fmt.Println(strings.HasSuffix("hello", "lo"))
+	//找查子符串第一次出现的位置，不存在返回-1
+	fmt.Println(strings.Index("hello", "el"))
+	//拼接字符串
+	fmt.Println(strings.Join([]string{"a", "b", "c"}, ","))
+	//重复字符串
+	fmt.Println(strings.Repeat("hello", 3))
+	//替换前n个字符串
+	fmt.Println(strings.Replace("hello", "l", "x", 1))
+	//替换所有字符串
+	fmt.Println(strings.Replace("hello", "l", "x", -1))
+
+	fmt.Printf("rrrr")
+	fmt.Printf("rrrr")
+	fmt.Printf("rrrr")
+	fmt.Println("line")
+	fmt.Println("line")
+	fmt.Println("line")
+	fmt.Println("line")
+
+	//格式化输出
+	var a = person{name: "test", age: 22}
+
+	//打印出结构体
+	fmt.Printf("%v\n", a)
+	//格式化输出Go语法表示方式
+	fmt.Printf("%#v\n", a)
+	//输出类型
+	fmt.Printf("%T\n", a)
+	//格式化布尔型变量
+	fmt.Printf("%t\n", true)
+	//输出整型
+	fmt.Printf("%d\n", 123)
+	//输出二进制
+	fmt.Printf("%b\n", 10)
+	//输出对应字符
+	fmt.Printf("%c\n", 65)
+	//输出十六进制表示
+	fmt.Printf("%x\n", 256)
+	//输出浮点数
+	fmt.Printf("%f\n", 12.45)
+	//科学计数法
+	fmt.Printf("%e\n", 1230000000.0)
+	fmt.Printf("%E\n", 1230000000.0)
+	//输出字符串
+	fmt.Printf("%s\n", "hello")
+	//输出指针的值
+	fmt.Printf("%p\n", &a)
+	//控制输出宽度，并用0补齐，输出默认右对齐的
+	fmt.Printf("%020d\n", 345)
+
+	//字符串截取 下表是从0开始
+	fmt.Println(sourceStr[0:8])
+
+}
