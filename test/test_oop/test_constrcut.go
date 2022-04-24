4b7448a0626ec209e189032be7db0cc9f6db1337
Rain <xzg323@163.com>
2022-04-24 23:56:20 +0800

    file read

---
+++ test/test_oop/test_constrcut.go
@@ -0,0 +1,49 @@
+package main
+
+import "fmt"
+
+// 定义一个名为Profile 的结构体
+type Profile struct {
+	name   string
+	age    int
+	gender string
+	mother *Profile // 指针
+	father *Profile // 指针
+}
+
+// 定义一个与 Profile 的绑定的方法
+func (person Profile) FmtProfile() {
+	fmt.Printf("名字：%s\n", person.name)
+	fmt.Printf("年龄：%d\n", person.age)
+	fmt.Printf("性别：%s\n", person.gender)
+}
+
+// 重点在于这个星号: *
+func (person *Profile) increase_age() {
+	person.age += 1
+}
+
+func main() {
+
+	// 实例化
+	myself := Profile{name: "小明", age: 24, gender: "male"}
+	// 调用函数
+	myself.FmtProfile()
+
+	myself.increase_age()
+	fmt.Printf("当前年龄：%d", myself.age)
+
+	p1 := &Profile{name: "iswbm"}
+	fmt.Println(p1.name) // output: iswbm
+
+	fmt.Println((*p1).name)
+
+	xm := new(Profile)
+	// 等价于: var xm *Profile = new(Profile)
+	fmt.Println(xm)
+	// output: &{ 0 }
+
+	xm.name = "iswbm222"
+
+	fmt.Println(xm.name) // output: iswbm
+}
