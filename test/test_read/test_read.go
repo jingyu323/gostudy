773b732475300c60f6962ebfa092a91b3f57fa0a
jingyu323 <xzg323@163.com>
2023-01-03 16:27:34 +0800

    read

--- test/test_read/test_read.go
+++ test/test_read/test_read.go
@@ -1,1 +1,36 @@
-package test_read
+package main
+
+import "fmt"
+
+func solution(n int, pages [7]int) {
+
+	var result int
+
+	// TODO: 请在此编写代码
+	for n > 0 {
+
+		for i := 0; i < len(pages); i++ {
+			result = i + 1
+			n = n - pages[i]
+			if n <= 0 {
+				break
+			}
+
+		}
+
+	}
+	fmt.Print(result)
+
+}
+
+func main() {
+	var n int
+	fmt.Scan(&n)
+	var pages [7]int
+
+	for i := 0; i < 7; i++ {
+		fmt.Scan(&pages[i])
+	}
+
+	solution(n, pages)
+}
